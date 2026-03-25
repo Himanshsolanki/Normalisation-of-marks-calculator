6ae20b637073536f95fdfcb362d717c44df8439a
Himansh Solanki <leohimansh10@gmail.com>
2026-03-25 17:26:09 +0000

    commit 2

--- Normalisation/app.py
+++ Normalisation/app.py
@@ -6,19 +6,23 @@ st.title("Marks Normalizer")
 st.write("Calculate your normalized score based on class performance.")
 
 #input
-marks = st.number_input("Enter the marks you have achieved in your paper : ", value = 0.0)
-max_val = st.number_input("Enter the max marks achieved in your class : ", value = 1.0)
-min_val = st.number_input("Enter the min marks achieved in your class : ", value = 0.0)
+marks = st.number_input("Enter the marks you have achieved in your paper : ", value = None, placeholder = "Enter Value")
+max_val = st.number_input("Enter the max marks achieved in your class : ", value = None, placeholder = "Enter Value")
+min_val = st.number_input("Enter the min marks achieved in your class : ", value = None, placeholder = "Enter Value")
 
-if max_val > min_val : 
-    normalised = (marks - min_val) / (max_val - min_val) *50
+if marks is not None and max_val is not None and min_val is not None : 
+    if max_val > min_val : 
+        normalised = (marks - min_val) / (max_val - min_val) *50
 
-    st.divider()
-    st.subheader("Final Result")
-    st.metric(label = "Normalised Score (Out of 50)", value = f"{normalised:.2f}")
+        st.divider()
+        st.subheader("Final Result")
+        st.metric(label = "Normalised Score (Out of 50)", value = f"{normalised:.2f}")
 
-else :
-    st.error("maximum marks cannot be less than minimum")
+    else :
+        st.error("maximum marks cannot be less than minimum")
+    
+else : 
+    st.warning("Please enter all values.")
 
 st.write("Created by Himansh Solanki")
 
